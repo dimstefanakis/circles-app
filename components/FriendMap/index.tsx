b8c8397c0d34fd8b5729c8b2817c1c454694f5c2
Dimitris <jimstef@outlook.com>
2023-12-14 23:15:41 +0200

    testing squarespace

--- components/FriendMap/index.tsx
+++ components/FriendMap/index.tsx
@@ -1,13 +1,51 @@
+import { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import { Button, Input, YStack, XStack } from 'tamagui';
 import { ChevronDown } from '@tamagui/lucide-icons';
 import { Dimensions } from 'react-native';
 import { useRouter } from 'expo-router';
+import useUser from '../../hooks/useUser';
+import * as Location from 'expo-location';
 
 const { width, height } = Dimensions.get('window');
 
 function FriendMap() {
   const router = useRouter();
+  const { user, session, loading } = useUser()
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState<string>();
+
+  async function onSearch(query: string) {
+    const response = await fetch(`${process.env.API_URL}/api/location-results`, {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        query: query,
+        ll: `${location?.coords.latitude},${location?.coords.longitude}`
+      }),
+      headers: {
+        'access_token': session?.access_token as string,
+        'refresh_token': session?.refresh_token as string,
+      }
+    })
+    const data = await response.json()
+
+    console.log(data, data.results, session?.user)
+  }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <YStack flex={1}>
       <XStack
@@ -18,7 +56,7 @@ function FriendMap() {
         w={width}
         justifyContent='space-between'
       >
-        <Input placeholder='Search' w={width - 80 }/>
+        <Input placeholder='Search' w={width - 80} onChangeText={onSearch} />
         <Button
           circular
           mx={2}
@@ -31,9 +69,15 @@ function FriendMap() {
       </XStack>
       <MapView
         style={{ flex: 1 }}
+        region={{
+          latitude: location?.coords.latitude || 37.78825,
+          longitude: location?.coords.longitude || -122.4324,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: location?.coords.latitude || 37.78825,
+          longitude: location?.coords.longitude || -122.4324,
           latitudeDelta: 0.1,
           longitudeDelta: 0.1,
         }}
